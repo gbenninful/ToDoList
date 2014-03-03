640c4591d09cbd7ab5abe92152d3fc8f92ef14ae
gbenninful <enninful@hotmail.com>
2014-03-03 00:35:36 -0800

    Initial Commit

--- ToDoList.js
+++ ToDoList.js
@@ -1,1 +1,26 @@
-﻿
+﻿var app = angular.module("toDoApp", ["ngRoute"])
+
+.controller("ToDoController", function ($scope) {
+    $scope.toDos = [
+        { text: "Learn Java", done: true },
+        { text: "Apply for jobs", done: false }];
+
+    $scope.remaining = function () {
+        var count = 0;
+        angular.forEach($scope.toDos, function (toDo) {
+            count += toDo.done ? 0 : 1;
+        });
+        return count;
+    };
+
+    $scope.addToDo = function () {
+        $scope.toDos.push({ text: $scope.toDo.text, done: false });
+        $scope.toDo.text = "";
+    };
+
+    //CONTINUE FROM HERE!!!
+    $scope.archive = function() {
+
+    }
+
+});
